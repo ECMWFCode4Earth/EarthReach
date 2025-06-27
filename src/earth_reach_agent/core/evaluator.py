b8393be9837b2d26c11b2ff7922c3fcb3a02e648
Romain Bazin <romain.bzin@gmail.com>
2025-06-27 15:02:09 -0400

    refact: improving todo item for clarity

--- src/earth_reach_agent/core/evaluator.py
+++ src/earth_reach_agent/core/evaluator.py
@@ -67,7 +67,7 @@ class CriterionEvaluator:
                 "Only one of 'figure' or 'image' can be provided, not both."
             )
         if figure is not None:
-            # TODO(medium): If metadata extraction failes, continue without it
+            # TODO(medium): If metadata extraction fails, continue without it
             metadata = self._get_metadata_from_figure(figure)
             self.user_prompt = self._update_user_prompt_with_metadata(
                 self.user_prompt, metadata
